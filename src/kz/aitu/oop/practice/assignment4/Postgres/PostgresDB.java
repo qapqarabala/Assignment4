8dcbc3f8bf2a7f6028b19a9bd1a16c42741146d3
qapqarabala <75445844+qapqarabala@users.noreply.github.com>
2021-02-22 11:29:37 +0600

    My OOP assignment 4 - company

--- src/kz/aitu/oop/practice/assignment4/Postgres/PostgresDB.java
+++ src/kz/aitu/oop/practice/assignment4/Postgres/PostgresDB.java
@@ -13,7 +13,7 @@ public class PostgresDB implements IDB {
             Class.forName("org.postgresql.Driver");
 
             // Establish the connection
-            Connection con = DriverManager.getConnection(connectionUrl, "postgres", "1234");
+            Connection con = DriverManager.getConnection(connectionUrl, "postgres", "password");
 
             return con;
         } catch (Exception e) {
